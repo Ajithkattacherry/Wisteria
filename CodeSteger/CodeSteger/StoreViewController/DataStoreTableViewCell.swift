9dd19380cdaf9b22742e97e5e5693ecf965f5a84
Ajith <ajith.antony@rapidvaluesolutions.com>
2015-09-13 23:10:36 +0530

    added base code structure

---
+++ CodeSteger/CodeSteger/StoreViewController/DataStoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DataStoreTableViewCell.swift
+//  CodeSteger
+//
+//  Created by Ajith on 13/09/15.
+//  Copyright (c) 2015 Code Poets. All rights reserved.
+//
+
+import UIKit
+
+class DataStoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ CodeSteger/CodeSteger/StoreViewController/DataStoreViewController.swift
@@ -0,0 +1,58 @@
+//
+//  DataStoreViewController.swift
+//  CodeSteger
+//
+//  Created by Ajith on 13/09/15.
+//  Copyright (c) 2015 Code Poets. All rights reserved.
+//
+
+import UIKit
+
+class DataStoreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    let cellIdentifier = "TextCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    //MARK: UITableView Datasource
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        
+        let row = indexPath.row
+        cell.textLabel?.text = "Test \(indexPath.row)"
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+    }
+}
